9328cbcf03a64de4cf9dfde1c6761cdd36ac823a
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-10-03 19:39:52 +1000

    Add AboutView

---
+++ iOS/Week3/Week3/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Week3
+//
+//  Created by Dawei Zhu on 2025/10/3.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("This is the about page.")
+    }
+}
+
+#Preview {
+    AboutView()
+}
