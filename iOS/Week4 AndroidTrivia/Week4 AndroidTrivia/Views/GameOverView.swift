f9cc72d831d0edcd60ed3555188ebc15b1ab6a43
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-10-03 22:30:02 +1000

    Add GameOverView

---
+++ iOS/Week4 AndroidTrivia/Week4 AndroidTrivia/Views/GameOverView.swift
@@ -0,0 +1,32 @@
+//
+//  GameOverView.swift
+//  Week4 AndroidTrivia
+//
+//  Created by Dawei Zhu on 2025/10/3.
+//
+
+import SwiftUI
+
+struct GameOverView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(.tryAgain)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .padding()
+                Button(.tryAgain) {
+                }.buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+                    .padding()
+            }.padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.gameOverBackground)
+    }
+}
+
+#Preview {
+    GameOverView()
+}
