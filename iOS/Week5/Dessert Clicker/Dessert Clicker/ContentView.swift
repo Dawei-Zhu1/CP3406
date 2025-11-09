d919954d31d5e52d4479df4babb3d5de6745729b
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-11-09 12:28:46 +0800

    Project initialization

---
+++ iOS/Week5/Dessert Clicker/Dessert Clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dessert Clicker
+//
+//  Created by Dawei Zhu on 2025/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(.bakeryBack).resizable().scaledToFill()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/Week5/Dessert Clicker/Dessert Clicker/Dessert_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_ClickerApp.swift
+//  Dessert Clicker
+//
+//  Created by Dawei Zhu on 2025/11/9.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
